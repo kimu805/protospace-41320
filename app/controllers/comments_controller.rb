ba4f7b3a99ac7167d528347459c945ee8626e03b
kimu805 <kimukimuok805@gmail.com>
2024-10-06 17:16:16 +0900

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,10 @@
 class CommentsController < ApplicationController
   def create
-    @comment = Comment.new(comment_params)
+    @prototype = Prototype.find(params[:prototype_id])
+    @comment = @prototype.comments.new(comment_params)
     if @comment.save
-      redirect_to prototype_path(@comment.prototype)
+      redirect_to prototype_path(@prototype)
     else
-      @prototype = @comment.prototype
       @comments = @prototype.comments.includes(:user)
       render "prototypes/show", status: :unprocessable_entity
     end
@@ -12,6 +12,6 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+    params.require(:comment).permit(:content).merge(user_id: current_user.id)
   end
 end
